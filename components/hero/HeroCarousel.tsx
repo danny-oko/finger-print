e8ae167d64d8a94fc779bb55d36aaae79136da1a
danny <dnioko0412@gmail.com>
2026-02-17 14:11:38 +0800

    hero scroll function added

--- components/hero/HeroCarousel.tsx
+++ components/hero/HeroCarousel.tsx
@@ -5,6 +5,7 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 import {
   Carousel,
+  CarouselApi,
   CarouselContent,
   CarouselItem,
   CarouselNext,
@@ -21,9 +22,25 @@ export default function HeroCarousel({
   slides: Slide[];
   className?: string;
 }) {
+  const [api, setApi] = React.useState<CarouselApi | null>(null);
+
+  React.useEffect(() => {
+    if (!api || slides.length <= 1) return;
+
+    const id = window.setInterval(() => {
+      api.scrollNext();
+    }, 6000);
+
+    return () => window.clearInterval(id);
+  }, [api, slides.length]);
+
   return (
     <section className={cn("w-full", className)}>
-      <Carousel opts={{ loop: true, align: "start" }} className="relative">
+      <Carousel
+        setApi={setApi}
+        opts={{ loop: true, align: "start" }}
+        className="relative"
+      >
         <CarouselContent className="m-0">
           {slides.map((slide, idx) => (
             <CarouselItem key={idx} className="p-0">
