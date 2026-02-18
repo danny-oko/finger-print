87eb3169cc13bb700aabc5ae53c36d618463664a
danny <dnioko0412@gmail.com>
2026-02-18 11:20:44 +0800

    Photo gallery title

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,19 +1,33 @@
 import React from "react";
-
 import DomeGallery from "./DomeGallery";
 
 const Gallery = () => {
   return (
-    <div className="w-full h-[80vh] object-fill">
-      <DomeGallery
-        fit={1}
-        minRadius={800}
-        maxVerticalRotationDeg={3}
-        segments={34}
-        dragDampening={1}
-        grayscale={false}
-      />
-    </div>
+    <section className="w-full">
+      <div className="mx-auto w-full pt-20 pb-10">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">
+            Photo Gallery
+          </h1>
+        </div>
+
+        <div className="relative mt-10 overflow-visible">
+          <div className="pointer-events-none absolute inset-x-0 top-0 h-28 bg-gradient-to-b from-white to-transparent" />
+          <div className="pointer-events-none absolute inset-x-0 bottom-0 h-28 bg-gradient-to-t from-white to-transparent" />
+
+          <div className="h-[85vh] min-h-[640px] w-full">
+            <DomeGallery
+              fit={1}
+              minRadius={520}
+              maxVerticalRotationDeg={6}
+              segments={34}
+              dragDampening={1}
+              grayscale={false}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
